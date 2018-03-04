5f220df20437454e610e03e4c090793fa7f49168
marcrabat <marc.rabat01@estudiant.upf.edu>
2018-03-04 16:16:11 +0100

    part 1 working, need to know what to do with under_scores

---
+++ P1PLN/p1nlp.py
@@ -0,0 +1,41 @@
+from collections import Counter
+from itertools import groupby
+
+
+
+def main():
+	
+	with open('corpus.txt', 'r') as training_set:	
+		model = generate_model(training_set)
+	training_set.close()
+
+	#print training_set.read() #treure output fitxer
+
+	
+
+def generate_model(training_set):
+	
+	ocurrencies_dict = count_ocurrencies(training_set)
+	generate_lexic_file(ocurrencies_dict)
+	
+
+
+def count_ocurrencies(training_set):
+	words_and_tags_dict = {}
+	for line in training_set:
+		aux = line.decode("latin-1").encode("UTF-8").split()
+		aux[0] = aux[0].lower()
+		string = str(aux[0] + " " + aux[1])
+		if string not in words_and_tags_dict:
+			words_and_tags_dict[string] = 1
+		else:
+			words_and_tags_dict[string] += 1
+	return words_and_tags_dict
+
+def generate_lexic_file(ocurrencies_dict):
+	with open('lexic.txt', 'w') as model:
+		for key,value in ocurrencies_dict.items():
+			format_to_print = str(key + " " + str(value) + "\n") 
+			model.write(format_to_print) 
+
+main()
