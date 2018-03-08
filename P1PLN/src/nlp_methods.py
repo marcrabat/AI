3b589508eb54467cdf7eb219625f12f02f75bc9f
marcrabat <marc.rabat01@estudiant.upf.edu>
2018-03-08 23:12:51 +0100

    Refactored

--- P1PLN/src/nlp_methods.py
+++ P1PLN/src/nlp_methods.py
@@ -5,17 +5,14 @@ import random
 
 def generate_model_from_training_set(path_to_training_set, output_filename):
 	with open(path_to_training_set, 'r') as training_set:	
-		write_model_to_file(training_set, output_filename)
+		ocurrencies_dict = count_ocurrencies(training_set)
+		with open(output_filename, 'w') as model:
+			for key,value in ocurrencies_dict.items():
+				format_to_print = str(key + " " + str(value) + "\n") 
+				format_to_print.decode("UTF-8").encode('latin-1')
+				model.write(format_to_print) 
+		model.close()
 	training_set.close()
-
-def write_model_to_file(training_set, output_filename):
-	ocurrencies_dict = count_ocurrencies(training_set)
-	with open(output_filename, 'w') as model:
-		for key,value in ocurrencies_dict.items():
-			format_to_print = str(key + " " + str(value) + "\n") 
-			format_to_print.decode("UTF-8").encode('latin-1')
-			model.write(format_to_print) 
-	model.close()
 
 
 def count_ocurrencies(training_set):
@@ -30,17 +27,6 @@ def count_ocurrencies(training_set):
 			words_and_tags_dict[string] += 1
 	return words_and_tags_dict
 
-def compute_accuracy(path_to_results, path_to_gold_standard):
-	results = load_evaluation_format(path_to_results)
-	gold_standard = load_evaluation_format(path_to_gold_standard)
-	count = 0.0
-	for i in xrange(len(results)):
-		if results.items()[i] == gold_standard.items()[i]:
-			count += 1
-		else:
-			print results.items()[i], "doesn't match", gold_standard.items()[i]
-	precision = (count / len(results)) * 100
-	print "ACCURACY: ", precision, "%"
 
 def load_evaluation_format(path):
 	with open(path, 'r') as eval:
@@ -50,31 +36,20 @@ def load_evaluation_format(path):
 			values[aux[0].lower()] = aux[1]
 		return values
 
-def generate_model_from_training_set(path_to_training_set, output_filename):
-	with open(path_to_training_set, 'r') as training_set:	
-		write_model_to_file(training_set, output_filename)
-	training_set.close()
+def compute_accuracy(path_to_results, path_to_gold_standard):
+	results = load_evaluation_format(path_to_results)
+	gold_standard = load_evaluation_format(path_to_gold_standard)
+	count = 0.0
+	for i in xrange(len(results)):
+		if results.items()[i] == gold_standard.items()[i]:
+			count += 1
+		if False:
+			print results.items()[i], "doesn't match", gold_standard.items()[i] #debug
+	
+	precision = (count / len(results)) * 100
+	print "\n\n"
+	print "ACCURACY: ", precision, "%"
 
-def write_model_to_file(training_set, output_filename):
-	ocurrencies_dict = count_ocurrencies(training_set)
-	with open(output_filename, 'w') as model:
-		for key,value in ocurrencies_dict.items():
-			format_to_print = str(key + " " + str(value) + "\n") 
-			format_to_print.decode("UTF-8").encode('latin-1')
-			model.write(format_to_print) 
-	model.close()
-
-def count_ocurrencies(training_set):
-	words_and_tags_dict = {}
-	for line in training_set:
-		aux = line.decode("latin-1").encode("UTF-8").split()
-		aux[0] = aux[0].lower()
-		string = str(aux[0] + " " + aux[1])
-		if string not in words_and_tags_dict:
-			words_and_tags_dict[string] = 1
-		else:
-			words_and_tags_dict[string] += 1
-	return words_and_tags_dict
 
 def load_model(path_to_model):
 	with open(path_to_model, 'r') as model:
@@ -106,8 +81,6 @@ def tag_with_model(path_to_test, model, output_filename):
 	test.close()
 
 def compute_prediction(word, model, default_gcs):
-	##MIRAR CAS EN QUE NO EXISTEIX EN EL MODEL
-
 	matches = []
 	word = word.decode("latin-1").encode("UTF-8") #needed to deal with accent problems
 	for key, value in model.items():
@@ -131,8 +104,6 @@ def compute_common_gcs(model):
 
 	#return most_common_gcs.keys()
 	return common_dict.keys()
-
-
 
 def compute_best_gc(words_matched):
 	best_score = 0
--- P1PLN/src/tagger.py
+++ P1PLN/src/tagger.py
@@ -4,21 +4,25 @@ from nlp_methods import *
 
 def main():
 	start_time = time.time()
+	print "Enter the # of test you want to prove:"
+	num = raw_input()
+	test = "test_" + str(num) + ".txt"
+	gold_standard = "gold_standard_" + str(num) + ".txt"
+	results = "results_" + str(num) + ".txt"
+
 	#PART 1
-	
 	print "Generating model from training_set..."
 	generate_model_from_training_set('corpus.txt', 'lexic.txt')
 	
 	#PART 2
 	print "Loading model from training_set..."
 	model = load_model('lexic.txt')
-	print "Tagging..."
-	tag_with_model("test_1.txt", model, 'results_1.txt')
-	#tag_with_model("test_2.txt", model, 'results_2.txt')
+	print "Tagging", test ,"..."
+	tag_with_model(test, model, results)
 	
 	#PART 3
-	print "Computing results..."
-	compute_accuracy('results_1.txt', 'gold_standard_1.txt')
+	print "Computing results of ", results , " vs. ", gold_standard, "..."
+	compute_accuracy(results, gold_standard)
 	print "--- %s seconds ---" % (time.time() - start_time)
 
 ########################################################################
